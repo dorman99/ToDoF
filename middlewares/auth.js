c27840d21606cb255c4dca67b353a93d4f00a32f
dorman99 <hafizh.abdillahhh@gmail.com>
2018-02-11 21:19:04 +0700

    edited done rata kiri

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 var jwt = require('jsonwebtoken');
 
 const authUser = (req,res,next)=>{
-
+    console.log(req.headers.accesstokenjwt,'ss')
     let decoded = jwt.verify(req.headers.accesstokenjwt,process.env.dormanSECRET)
-    console.log(decoded,'masuk')
+   
     req.headers.userId = decoded.id
     if(decoded){
+        console.log('masuk sini ga?asass')
         next()
     }
     
